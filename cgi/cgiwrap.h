1911202953bbd2cac90cadb8e5bb1be0d40b60f2
blong <blong@853ec0fc-c9ed-11de-8410-e5b1e99fc78e>
2000-12-29 01:12:39 +0000

    First checkin of cgi handlers

---
+++ cgi/cgiwrap.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2000 - Neotonic
+ */
+
+/*
+ * cgiwrap.h
+ * The purpose of the cgiwrap is to abstract the CGI interface to allow
+ * for other than the default implementation.  The default
+ * implementation is of course based on environment variables and stdio,
+ * but this can be used with server modules which can substitute their
+ * own implementation of these functions.
+ */
+
+
+#ifndef __CGIWRAP_H_
+#define __CGIWRAP_H_ 1
+
+#include <stdargs.h>
+#include "neo_err.h";
+
+typedef int (*READ_FUNC)(void *, char *, int);
+typedef int (*WRITEF_FUNC)(void *, char *, va_list);
+typedef int (*WRITE_FUNC)(void *, char *, int);
+typedef char * (*GETENV_FUNC)(void *, char *);
+typedef int (*PUTENV_FUNC)(void *, char *, char *);
+typedef char * (*ITERENV_FUNC)(void *, int, char **, char **);
+
+NEOERR *cgiwrap_init_std (int argc, char **argv, char **envp);
+NEOERR *cgiwrap_init_emu (void *data, READ_FUNC *read_cb, 
+    WRITEF_FUNC *writef_cb, WRITE_FUNC *write_cb, GETENV_FUNC *getenv_cb,
+    PUTENV_FUNC *putenv_cb, ITERENV_FUNC *iterenv_cb);
+NEOERR *cgiwrap_getenv (char *k, char **v);
+NEOERR *cgiwrap_putenv (char *k, char *v);
+NEOERR *cgiwrap_iterenv (int start, char **k, char **v);
+NEOERR *cgiwrap_writef (char *fmt, ...);
+NEOERR *cgiwrap_write (char *buf, int buf_len);
+NEOERR *cgiwrap_read (char *buf, int buf_len);
+
+#endif /* __CGIWRAP_H_ */
