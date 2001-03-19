186674fc6410a8d776b8f8124f321ed1f759eb73
blong <blong@853ec0fc-c9ed-11de-8410-e5b1e99fc78e>
2001-03-19 01:03:21 +0000

    add handling of cookies add redirects handle empty vars in query string

--- cgi/cgi.h
+++ cgi/cgi.h
@@ -5,8 +5,11 @@
 #ifndef __CGI_H_ 
 #define __CGI_H_ 1
 
+#include <stdarg.h>
 #include "util/neo_err.h"
 #include "util/neo_hdf.h"
+
+extern int CGIFinished;
 
 typedef struct _cgi
 {
@@ -23,5 +26,14 @@ NEOERR *cgi_display (CGI *cgi, char *cs_file);
 void cgi_neo_error (CGI *cgi, NEOERR *err);
 void cgi_error (CGI *cgi, char *fmt, ...);
 void cgi_debug_init (int argc, char **argv);
+NEOERR *cgi_url_escape (char *buf, char **esc);
+void cgi_redirect (CGI *cgi, char *fmt, ...);
+void cgi_redirect_uri (CGI *cgi, char *fmt, ...);
+void cgi_vredirect (CGI *cgi, int uri, char *fmt, va_list ap);
+
+char *cgi_cookie_authority (CGI *cgi, char *host);
+NEOERR *cgi_cookie_set (CGI *cgi, char *name, char *value, char *path, 
+    char *domain, char *time_str, int persistant);
+NEOERR *cgi_cookie_clear (CGI *cgi, char *name, char *domain, char *path);
 
 #endif /* __CGI_H_ */
