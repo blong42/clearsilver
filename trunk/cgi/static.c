a762fd36bbc8cb122ea41e405b14a449840dc6fe
blong <blong@853ec0fc-c9ed-11de-8410-e5b1e99fc78e>
2001-05-18 22:12:04 +0000

    add the files path to the load path add debugging output check for various HDF files

--- trunk/cgi/static.c
+++ trunk/cgi/static.c
@@ -1,6 +1,9 @@
 
 #include <unistd.h>
 #include <limits.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "cgi/cgi.h"
 #include "cgi/cgiwrap.h"
 #include "util/neo_err.h"
@@ -11,6 +14,8 @@ int main (int argc, char **argv, char **envp)
   CGI *cgi;
   char *cs_file;
   char hdf_file[_POSIX_PATH_MAX];
+  char *p;
+  int x;
 
   cgi_debug_init (argc,argv);
   cgiwrap_init_std (argc, argv, envp);
@@ -29,13 +34,40 @@ int main (int argc, char **argv, char **envp)
     cgi_error (cgi, "No PATH_TRANSLATED var");
     return -1;
   }
+  p = strrchr (cs_file, '/');
+  if (p)
+  {
+    *p = '\0';
+    err = hdf_set_value (cgi->hdf, "hdf.loadpaths.0", cs_file);
+    *p = '/';
+    if (err)
+    {
+      cgi_neo_error(cgi, err);
+      nerr_log_error(err);
+      return -1;
+    }
+  }
   snprintf (hdf_file, sizeof(hdf_file), "%s.hdf", cs_file);
   err = hdf_read_file (cgi->hdf, hdf_file);
-  if (err != STATUS_OK)
+  if (err && !nerr_handle(&err, NERR_NOT_FOUND))
   {
     cgi_neo_error(cgi, err);
     nerr_log_error(err);
     return -1;
+  }
+  p = strrchr (cs_file, '.');
+  if (p)
+  {
+    *p = '\0';
+    snprintf (hdf_file, sizeof(hdf_file), "%s.hdf", cs_file);
+    *p = '.';
+    err = hdf_read_file (cgi->hdf, hdf_file);
+    if (err && !nerr_handle(&err, NERR_NOT_FOUND))
+    {
+      cgi_neo_error(cgi, err);
+      nerr_log_error(err);
+      return -1;
+    }
   }
   err = cgi_display (cgi, cs_file);
   if (err != STATUS_OK)
@@ -43,6 +75,28 @@ int main (int argc, char **argv, char **envp)
     cgi_neo_error(cgi, err);
     nerr_log_error(err);
     return -1;
+  }
+  if (hdf_get_int_value (cgi->hdf, "Query.debug", 0))
+  {
+    cgiwrap_writef("<hr>");
+    x = 0;
+    while (1)
+    {
+      char *k, *v;
+      err = cgiwrap_iterenv (x, &k, &v);
+      if (err != STATUS_OK)
+      {
+	nerr_log_error(err);
+	break;
+      }
+      if (k == NULL) break;
+      cgiwrap_writef("%s = %s<br>", k, v);
+      free(k);  
+      free(v);
+      x++;
+    }
+    cgiwrap_writef("<pre>");
+    hdf_dump(cgi->hdf, NULL);
   }
   return 0;
 }
