f9ddc9450f65074a96dc04a52a591a75dfab78dd
blong <blong@853ec0fc-c9ed-11de-8410-e5b1e99fc78e>
2001-06-29 08:00:36 +0000

    handle file upload and form-data upload

--- cgi/cgi.h
+++ cgi/cgi.h
@@ -22,12 +22,24 @@ typedef struct _cgi
   double time_end;
   void *data;
   HDF *hdf;
+
+  /* For line oriented reading of form-data input */
+  char *buf;
+  int buflen;
+  int readlen;
+  BOOL found_nl;
+  BOOL unget;
+  int nl;
+
+  /* this is a list of filepointers pointing at files that were uploaded */
+  ULIST *files;
 } CGI;
 
 NEOERR *cgi_init (CGI **cgi, char *hdf_file);
 void cgi_destroy (CGI **cgi);
 NEOERR *cgi_parse (CGI *cgi);
 NEOERR *cgi_display (CGI *cgi, char *cs_file);
+FILE *cgi_filehandle (CGI *cgi, char *form_name);
 void cgi_neo_error (CGI *cgi, NEOERR *err);
 void cgi_error (CGI *cgi, char *fmt, ...);
 void cgi_debug_init (int argc, char **argv);
@@ -40,5 +52,8 @@ char *cgi_cookie_authority (CGI *cgi, char *host);
 NEOERR *cgi_cookie_set (CGI *cgi, char *name, char *value, char *path, 
     char *domain, char *time_str, int persistant);
 NEOERR *cgi_cookie_clear (CGI *cgi, char *name, char *domain, char *path);
+
+/* internal use only */
+NEOERR * parse_rfc2388 (CGI *cgi);
 
 #endif /* __CGI_H_ */
