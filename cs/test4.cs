692c34aac6e24f58f3245e9a979d3bc076d32c71
blong <blong@853ec0fc-c9ed-11de-8410-e5b1e99fc78e>
2001-07-04 00:43:05 +0000

    fix bug where we treated > as <

--- cs/test4.cs
+++ cs/test4.cs
@@ -22,3 +22,29 @@ Testing cs set
 <?cs /each ?>
 
 <?cs var:Set.1.5 ?>
+
+<?cs if:#5 % #2 ?>
+ 5 % 2 == true
+<?cs else ?>
+ERROR! um, wrong?
+ 5 % 2 == false?
+<?cs /if ?>
+
+<?cs if:#4 % #2 ?>
+ um, wrong?
+ERROR! 4 % 2 == true?
+<?cs else ?>
+ 4 % 2 == false
+<?cs /if ?>
+
+<?cs if:#4 > #0 ?>
+  Yes, 4 > 0
+<?cs else ?>
+ERROR!  Wrong, 4 is not < 0
+<?cs /if ?>
+
+<?cs if:#4 < #0 ?>
+ERROR!  4 > 0
+<?cs else ?>
+right, 4 > 0
+<?cs /if ?>
