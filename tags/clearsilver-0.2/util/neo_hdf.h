69cb3ae8c486557c4c4f53748f0990915e77f113
nobody <nobody@853ec0fc-c9ed-11de-8410-e5b1e99fc78e>
2001-08-21 02:39:18 +0000

    This commit was manufactured by cvs2svn to create tag 'clearsilver-0.2'.

---
+++ tags/clearsilver-0.2/util/neo_hdf.h
@@ -0,0 +1,68 @@
+/*
+ * Neotonic ClearSilver Templating System
+ *
+ * This code is made available under the terms of the 
+ * Neotonic ClearSilver License.
+ * http://www.neotonic.com/clearsilver/license.hdf
+ *
+ * Copyright (C) 2001 by Brandon Long
+ */
+
+#ifndef __NEO_HDF_H_
+#define __NEO_HDF_H_ 1
+
+__BEGIN_DECLS
+
+#include <stdio.h>
+#include "neo_err.h"
+#include "ulist.h"
+
+typedef struct _hdf
+{
+  int top;
+  int alloc_value;
+  char *name;
+  int name_len;
+  char *value;
+  struct _hdf *next;
+  struct _hdf *child;
+} HDF;
+
+NEOERR* hdf_init (HDF **hdf);
+void hdf_destroy (HDF **hdf);
+
+int hdf_get_int_value (HDF *hdf, char *name, int defval);
+char *hdf_get_value (HDF *hdf, char *name, char *defval);
+NEOERR* hdf_get_copy (HDF *hdf, char *name, char **value, char *defval);
+
+HDF* hdf_get_obj (HDF *hdf, char *name);
+HDF* hdf_get_child (HDF *hdf, char *name);
+HDF* hdf_obj_child (HDF *hdf);
+HDF* hdf_obj_next (HDF *hdf);
+char* hdf_obj_name (HDF *hdf);
+char* hdf_obj_value (HDF *hdf);
+
+NEOERR* hdf_set_value (HDF *hdf, char *name, char *value);
+NEOERR* hdf_set_int_value (HDF *hdf, char *name, int value);
+NEOERR* hdf_set_copy (HDF *hdf, char *dest, char *src);
+NEOERR* hdf_set_buf (HDF *hdf, char *name, char *value);
+
+NEOERR* hdf_read_file (HDF *hdf, char *path);
+NEOERR* hdf_write_file (HDF *hdf, char *path);
+
+NEOERR* hdf_read_string (HDF *hdf, char *s);
+NEOERR* hdf_write_string (HDF *hdf, char **s);
+
+NEOERR* hdf_dump (HDF *hdf, char *prefix);
+NEOERR* hdf_dump_format (HDF *hdf, int lvl, FILE *fp);
+NEOERR* hdf_dump_str(HDF *hdf, char *prefix, int compact, STRING *str);
+
+NEOERR* hdf_remove_tree (HDF *hdf, char *name);
+NEOERR* hdf_copy (HDF *dest_hdf, char *name, HDF *src);
+
+/* Utility function */
+NEOERR* hdf_search_path (HDF *hdf, char *path, char *full);
+
+__END_DECLS
+
+#endif /* __NEO_HDF_H_ */
---
+++ tags/clearsilver-0.2/util/neo_test.c
@@ -0,0 +1,93 @@
+/*
+ * Neotonic ClearSilver Templating System
+ *
+ * This code is made available under the terms of the 
+ * Neotonic ClearSilver License.
+ * http://www.neotonic.com/clearsilver/license.hdf
+ *
+ * Copyright (C) 2001 by Brandon Long
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include "neo_err.h"
+#include "neo_misc.h"
+#include "ulist.h"
+
+static int RandomInit = 0;
+
+void neot_seed_rand (long int seed)
+{
+  ne_warn ("Rand Seed is %ld", seed);
+  srand48(seed);
+  RandomInit = 1;
+}
+
+int neot_rand (int max)
+{
+  int r;
+
+  if (RandomInit == 0)
+  {
+    neot_seed_rand (time(NULL));
+  }
+  r = drand48() * max;
+  return r;
+}
+
+int neot_rand_string (char *s, int max)
+{
+  int size;
+  int x = 0;
+
+  size = neot_rand(max-1);
+  for (x = 0; x < size; x++)
+  {
+    s[x] = (char)(32 + neot_rand(127-32));
+    if (s[x] == '/') s[x] = ' ';
+  }
+  s[x] = '\0';
+
+  return 0;
+}
+
+static ULIST *Words = NULL;
+
+int neot_rand_word (char *s, int max)
+{
+  NEOERR *err;
+  int x;
+  char *word;
+
+  if (Words == NULL)
+  {
+    FILE *fp;
+    char buf[256];
+
+    err = uListInit(&Words, 40000, 0);
+    if (err) 
+    {
+      nerr_log_error(err);
+      return -1;
+    }
+    fp = fopen ("/usr/dict/words", "r");
+    while (fgets (buf, sizeof(buf), fp) != NULL)
+    {
+      x = strlen (buf);
+      if (buf[x-1] == '\n')
+	buf[x-1] = '\0';
+      uListAppend(Words, strdup(buf));
+    }
+    fclose (fp);
+  }
+  x = neot_rand (uListLength(Words));
+  uListGet(Words, x, (void *)&word);
+  strncpy (s, word, max);
+  s[max-1] = '\0';
+
+  return 0;
+}
+
