a79931b41e8a52c308203ddf43cc6f96b43f8eef
jeske <jeske@853ec0fc-c9ed-11de-8410-e5b1e99fc78e>
2002-04-13 18:58:07 +0000

    fixes for stat build

--- python/p_neo_util.h
+++ python/p_neo_util.h
@@ -11,10 +11,23 @@
 #ifndef __P_NEO_UTIL_H_
 #define __P_NEO_UTIL_H_ 1
 
+#include <util/neo_hdf.h>
+
+__BEGIN_DECLS
+
+// * external HDF object interface.
+
+PyObject * p_hdf_to_object (HDF *data, int dealloc);
+HDF * p_object_to_hdf (PyObject *ho);
+
+// other functions
+
 PyObject * p_neo_error (NEOERR *err);
 PyObject * p_hdf_to_object (HDF *data, int dealloc);
 HDF * p_object_to_hdf (PyObject *ho);
 void initneo_util(void);
 void initneo_cs(void);
+
+__END_DECLS
 
 #endif /* __P_NEO_UTIL_H_ */
