e8e0cc838eb0c6269073e0672fcd52e51912b309
jeske <jeske@853ec0fc-c9ed-11de-8410-e5b1e99fc78e>
2002-04-18 21:42:33 +0000

    make neo_date.h C++ safe

--- util/neo_date.h
+++ util/neo_date.h
@@ -11,10 +11,14 @@
 #ifndef _NEO_DATE_H_
 #define _NEO_DATE_H_ 1
 
+__BEGIN_DECLS
+
 /* UTC time_t -> struct tm in local timezone */
 void neo_time_expand (const time_t tt, char *timezone, struct tm *ttm);
 
 /* local timezone struct tm -> time_t UTC */
 time_t neo_time_compact (struct tm *ttm, char *timezone);
+
+__END_DECLS
 
 #endif /* _NEO_DATE_H_ */
