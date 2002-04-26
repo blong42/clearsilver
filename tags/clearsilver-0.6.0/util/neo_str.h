0770d1dc3961204dc1adf510c4ccce21ed51e5d8
nobody <nobody@853ec0fc-c9ed-11de-8410-e5b1e99fc78e>
2002-04-26 01:58:14 +0000

    This commit was manufactured by cvs2svn to create tag 'clearsilver-0.6.0'.

---
+++ tags/clearsilver-0.6.0/util/neo_str.h
@@ -0,0 +1,70 @@
+
+/*
+ * Neotonic ClearSilver Templating System
+ *
+ * This code is made available under the terms of the 
+ * Neotonic ClearSilver License.
+ * http://www.neotonic.com/clearsilver/license.hdf
+ *
+ * Copyright (C) 2001 by Brandon Long
+ */
+
+#ifndef __NEO_STR_H_
+#define __NEO_STR_H_ 1
+
+__BEGIN_DECLS
+
+#include <stdarg.h>
+#include <stdio.h>
+#include "util/neo_misc.h"
+
+/* This modifies the string its called with by replacing all the white
+ * space on the end with \0, and returns a pointer to the first
+ * non-white space character in the string 
+ */
+char *neos_strip (char *s);
+
+void neos_lower (char *s);
+
+char *sprintf_alloc (char *fmt, ...);
+char *vsprintf_alloc (char *fmt, va_list ap);
+
+typedef struct _string
+{
+  UINT8 *buf;
+  int len;
+  int max;
+} STRING;
+
+typedef struct _string_array
+{
+  char **entries;
+  int count;
+  int max;
+} STRING_ARRAY;
+
+void string_init (STRING *str);
+NEOERR *string_set (STRING *str, char *buf);
+NEOERR *string_append (STRING *str, char *buf);
+NEOERR *string_appendn (STRING *str, char *buf, int l);
+NEOERR *string_append_char (STRING *str, char c);
+NEOERR *string_appendf (STRING *str, char *fmt, ...);
+NEOERR *string_appendvf (STRING *str, char *fmt, va_list ap);
+NEOERR *string_readline (STRING *str, FILE *fp);
+void string_clear (STRING *str);
+
+/* typedef struct _ulist ULIST; */
+#include "ulist.h"
+NEOERR *string_array_split (ULIST **list, char *s, char *sep, int max);
+
+BOOL reg_search (char *re, char *str);
+
+
+NEOERR* neos_escape(UINT8 *buf, int buflen, char esc_char, char *escape, char **esc);
+UINT8 *neos_unescape (UINT8 *s, int buflen, char esc_char);
+
+char *repr_string_alloc (char *s);
+
+__END_DECLS
+
+#endif /* __NEO_STR_H_ */
---
+++ tags/clearsilver-0.6.0/util/rcfs.h
@@ -0,0 +1,32 @@
+/*
+ * Neotonic ClearSilver Templating System
+ *
+ * This code is made available under the terms of the 
+ * Neotonic ClearSilver License.
+ * http://www.neotonic.com/clearsilver/license.hdf
+ *
+ * Copyright (C) 2001 by Brandon Long
+ */
+
+/*
+ * revision-controlled file system (RCFS) with meta-info storage
+ */
+#ifndef __RCFS_H_
+#define __RCFS_H_ 1
+
+typedef struct _rcfs RCFS;
+
+NEOERR * rcfs_init (RCFS **rcfs);
+NEOERR * rcfs_destroy (RCFS **rcfs);
+
+NEOERR * rcfs_load (char *path, int version, char **data);
+NEOERR * rcfs_save (char *path, char *data, char *user, char *log);
+NEOERR * rcfs_lock (char *path, int *lock);
+void rcfs_unlock (int lock);
+NEOERR * rcfs_meta_load (char *path, HDF **meta);
+NEOERR * rcfs_meta_save (char *path, HDF *meta);
+NEOERR * rcfs_listdir (char *path, ULIST **list);
+NEOERR * rcfs_link (char *src_path, char *dest_path);
+NEOERR * rcfs_unlink (char *path);
+
+#endif /* __RCFS_H_ */
