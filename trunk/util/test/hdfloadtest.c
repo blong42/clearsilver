4fdf89594703772cedf99dbaa9a4bf21f98e1c89
blong <blong@853ec0fc-c9ed-11de-8410-e5b1e99fc78e>
2002-06-05 01:26:50 +0000

    some changes so we can test specific files

--- trunk/util/test/hdfloadtest.c
+++ trunk/util/test/hdfloadtest.c
@@ -8,9 +8,17 @@ int main(int argc, char *argv[])
   NEOERR *err;
   HDF *hdf;
   int x;
-  char name[256];
-  char value[256];
   double tstart = 0;
+  char *file;
+  int reps = 1000;
+
+  if (argc > 1)
+    file = argv[1];
+  else
+    file = "test.hdf";
+
+  if (argc > 2)
+    reps = atoi(argv[2]);
 
   err = hdf_init(&hdf);
   if (err != STATUS_OK) 
@@ -19,16 +27,19 @@ int main(int argc, char *argv[])
     return -1;
   }
 
-  for (x = 0; x < 1000; x++)
+  tstart = ne_timef();
+
+  for (x = 0; x < reps; x++)
   {
-    err = hdf_read_file (hdf, "test.hdf");
+    err = hdf_read_file (hdf, file);
     if (err != STATUS_OK) 
     {
       nerr_log_error(err);
       return -1;
     }
   }
-  hdf_dump(hdf, NULL);
+  ne_warn("Load test finished in %5.3fs", ne_timef() - tstart);
+  /* hdf_dump(hdf, NULL); */
 
   hdf_destroy(&hdf);
 
