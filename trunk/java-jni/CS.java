cf9199d5e48572fb3d7e324f2282fbdf8fe6e782
jeske <jeske@853ec0fc-c9ed-11de-8410-e5b1e99fc78e>
2002-09-20 05:55:30 +0000

    first version of java clearsilver wrapper

---
+++ trunk/java-jni/CS.java
@@ -0,0 +1,42 @@
+package org.clearsilver;
+
+import java.io.*;
+import java.util.*;
+
+public class CS {
+  public int csptr;
+  
+  static { 
+    try {
+      System.loadLibrary("jni_neo_cgi");
+    } catch ( UnsatisfiedLinkError e ) {
+      System.out.println("Could not load libjni_neo_cgi.so");
+      System.exit(1);
+    }
+  }
+  
+  public CS(HDF ho) {
+    csptr = _init(ho.hdfptr);
+  }
+  public void finalize() {
+    _dealloc(csptr);
+  }
+
+  public void parseFile(String filename) {
+    _parseFile(csptr,filename);
+  }
+
+  public void parseStr(String content) {
+    _parseStr(csptr,content);
+  }
+
+  public String render() {
+    return _render(csptr);
+  }
+  
+  private native int    _init(int ptr);
+  private native void   _dealloc(int ptr);
+  private native void   _parseFile(int ptr,String filename);
+  private native void   _parseStr(int ptr, String content);
+  private native String _render(int ptr);
+};
