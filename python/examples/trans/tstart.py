0b2b32606086617c49335d4b28fc474178d0d4e8
jeske <jeske@853ec0fc-c9ed-11de-8410-e5b1e99fc78e>
2003-08-17 16:50:07 +0000

    new location of base code

--- python/examples/trans/tstart.py
+++ python/examples/trans/tstart.py
@@ -5,16 +5,9 @@
 
 import sys
 
-ROOT_DIR = "../"
-sys.path.insert(0, ROOT_DIR)
+sys.path.insert(0, "../base") # pickup base libs
 
 sys.path.insert(0, "../../") # pickup neo_cgi.so
-
-try:
-  from paths import paths
-  sys.path = paths(ROOT_DIR) + sys.path
-except:
-  pass
 
 # don't put anything above this because the path isn't
 # extended yet...
