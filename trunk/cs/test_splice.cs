e74809c139691bd21f69e22857ff3453be0c9db8
blong <blong@853ec0fc-c9ed-11de-8410-e5b1e99fc78e>
2004-02-27 02:15:20 +0000

    put back having gt/lt coerce to numeric, and fix test rename of splice to slice

--- trunk/cs/test_splice.cs
+++ trunk/cs/test_splice.cs
@@ -2,7 +2,7 @@
 <?cs var:Foo ?>
 <?cs loop:b=0,10 ?>
 <?cs loop:e=10,0,-1 ?>
-<?cs var:string.splice(Foo, b, e) ?>
+<?cs var:string.slice(Foo, b, e) ?>
 <?cs /loop ?>
 <?cs /loop ?>
-<?cs var:string.splice(Foo, -5, -1) ?>
+<?cs var:string.slice(Foo, -5, -1) ?>
