cefc0e1f7beb62c9a0fca5ccabcaa43a1f53358a
blong <blong@853ec0fc-c9ed-11de-8410-e5b1e99fc78e>
2005-01-11 02:17:48 +0000

    add -lcgi to ruby

--- trunk/ruby/ext/hdf/extconf.rb
+++ trunk/ruby/ext/hdf/extconf.rb
@@ -5,6 +5,8 @@ require 'mkmf'
 # dir_config("hdf","../../..","../../../libs")
 dir_config("hdf")
 
-if have_header("ClearSilver.h") && have_library("neo_utl","hdf_init") && have_library("neo_cs","cs_init")
+have_library("z", "deflate")
+
+if have_header("ClearSilver.h") && have_library("neo_utl","hdf_init") && have_library("neo_cs","cs_init") && have_library("neo_cgi","cgi_register_strfuncs")
   create_makefile("hdf")
 end
