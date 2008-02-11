ac342411ba05c2d127a57d0307c70263a2c927ea
blong <blong@853ec0fc-c9ed-11de-8410-e5b1e99fc78e>
2008-02-11 15:04:19 +0000

    Add new library (libneo_htmlparser) to the rest of the wrappers.
    TESTED=compiled

--- trunk/ruby/ext/hdf/extconf.rb
+++ trunk/ruby/ext/hdf/extconf.rb
@@ -7,6 +7,6 @@ dir_config("hdf")
 
 have_library("z", "deflate")
 
-if have_header("ClearSilver.h") && have_library("neo_utl","hdf_init") && have_library("neo_cs","cs_init") && have_library("neo_cgi","cgi_register_strfuncs")
+if have_header("ClearSilver.h") && have_library("neo_htmlparser", "htmlparser_new") && have_library("neo_utl","hdf_init") && have_library("neo_cs","cs_init") && have_library("neo_cgi","cgi_register_strfuncs")
   create_makefile("hdf")
 end
