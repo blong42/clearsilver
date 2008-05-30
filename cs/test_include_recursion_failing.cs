f57950020c22c8c5707714adb2234cb57f977ed9
mikaelm <mikaelm@853ec0fc-c9ed-11de-8410-e5b1e99fc78e>
2008-05-30 01:16:56 +0000

    ClearSilver: Tests for failing and passing recursion in include/linclude/macros added. Changed the Makefile to be able to handle failing tests. Changed cstest.c by adding one possible flag : -parse_must_fail
    Removed a unused variable in csparse.c

---
+++ cs/test_include_recursion_failing.cs
@@ -0,0 +1,3 @@
+This line should be shown
+<?cs include:"test_include_recursion_failing.cs" ?>
+but not this line. It should raise an error.
---
+++ cs/test_macro_recursion.cs
@@ -0,0 +1,9 @@
+Here we begin with a macro recursion.
+<?cs def:map_val(val) ?>
+  <?cs var:val ?>
+  <?cs if:val<200 ?> 
+    <?cs call:map_val(val + 1) ?>
+  <?cs /if ?>
+<?cs /def ?>
+<?cs call:map_val(#0) ?>
+End of the macro recursion with no problems.
