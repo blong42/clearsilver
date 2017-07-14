43deeca7b0b0930df5635e7e8b83b38a2d69f3ee
Brandon Long <blong@fiction.net>
2017-07-14 16:06:06 -0700

    Support parsing of posted data if the content-type header has parameters.

--- cgi/cgi.c
+++ cgi/cgi.c
@@ -832,7 +832,9 @@ NEOERR *cgi_parse (CGI *cgi)
 
   if (!strcmp(method, "POST"))
   {
-    if (type && !strcmp(type, "application/x-www-form-urlencoded"))
+    if (type &&
+        (!strcmp(type, "application/x-www-form-urlencoded") ||
+        (!strncmp(type, "application/x-www-form-urlencoded;", 34))))
     {
       err = _parse_post_form(cgi);
       if (err != STATUS_OK) return nerr_pass (err);
